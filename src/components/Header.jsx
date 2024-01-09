ba2191a7b99f7316446f30727d3c2220cf98c4b0
harsharma2026 <harsharma2026@gmail.com>
2024-01-10 01:17:20 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <div className="grid grid-flow-col px-7 text-black font-bold text-[30px] w-full bg-gray-100 font-serif h-[90px]">
+            <div className="grid justify-self-start self-center">
+                Admin Dashboard
+            </div>
+        </div>
+    )
+}
